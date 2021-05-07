b266314d82c34c6efab0a23eb9827d9fe7b57df2
Layiri Batiene <eratos02@yahoo.fr>
2021-05-07 04:23:25 +0300

    add helpers, database configuration, and image generators

--- banner.php
+++ banner.php
@@ -1,1 +1,19 @@
 <?php
+
+
+
+
+$my_img = imagecreate(400, 80);
+$background = imagecolorallocate($my_img, 0, 0, 255);
+$text_color = imagecolorallocate($my_img, 255, 255, 0);
+$line_color = imagecolorallocate($my_img, 128, 255, 0);
+imagestring($my_img, 4, 30, 25, "This image was created by Layiri Technologies", $text_color);
+imagesetthickness($my_img, 5);
+imageline($my_img, 30, 45, 380, 45, $line_color);
+header("Content-type: image/png");
+imagepng($my_img);
+imagecolordeallocate($line_color);
+imagecolordeallocate($text_color);
+imagecolordeallocate($background);
+imagedestroy($my_img);
+
---
+++ config/config.php
@@ -0,0 +1,13 @@
+<?php
+
+
+return $config = [
+    'db' => [
+        'host' => 'localhost',
+        'port' => '3308',
+        'dbname' => 'infuse',
+        'username' => 'root',
+        'password' => '',
+        'charset' => 'utf8',
+    ],
+];
